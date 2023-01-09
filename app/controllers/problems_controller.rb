d4c4addde22ae03603c060d8a81e9578e7dd4441
Airan Telles <airan.telles@bravosul.com.br>
2023-01-09 01:05:49 -0300

    Resolved problem 6

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -101,4 +101,11 @@ class ProblemsController < ApplicationController
     result.empty?
   end
 
+  # 136. Single Number
+  # Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+  # You must implement a solution with a linear runtime complexity and use only constant extra space.
+  def self.single_number(nums)
+    (nums.uniq.sum * 2) - nums.sum
+  end
+
 end
