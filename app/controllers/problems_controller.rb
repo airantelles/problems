7d7f0a05bbade9c2eb06bdb0a21de8b1f7ed1b35
Airan Telles <airan.telles@bravosul.com.br>
2023-01-09 00:06:01 -0300

    Resolved problem 4

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -65,4 +65,21 @@ class ProblemsController < ApplicationController
     end
   end
 
+  # 7. Reverse Integer
+  # Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+  # Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+  def self.reverse(x)
+    exponential = 2**31
+    if x.positive?
+      result = x.to_s.reverse.to_i
+    else
+      result = -x.to_s.reverse.to_i
+    end
+    if result.to_i < -exponential || result.to_i > exponential - 1
+      return 0
+    else
+      return result.to_i
+    end
+  end
+
 end
