c62dfcda0c53e516e9185c38c4380cc56cc0a645
Airan Telles <airan.telles@bravosul.com.br>
2023-01-09 00:34:33 -0300

    Resolved problem 5

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -82,4 +82,23 @@ class ProblemsController < ApplicationController
     end
   end
 
+  # 20. Valid Parentheses
+  # Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+  # An input string is valid if:
+  # Open brackets must be closed by the same type of brackets.
+  # Open brackets must be closed in the correct order.
+  # Every close bracket has a corresponding open bracket of the same type.
+
+  # @param {String} s
+  # @return {Boolean}
+  def self.is_valid(s)
+    characters = { '(' => ')', '{' => '}', '[' => ']' }
+    result = []
+    s.each_char do |char|
+      result << char if characters.key?(char)
+      return false if characters.key(char) && characters.key(char) != result.pop
+    end
+    result.empty?
+  end
+
 end
