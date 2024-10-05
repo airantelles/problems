fe9a532fb0461f59e593a55a20e2e34544d3f348
Airan Telles <airan.telles@bravosul.com.br>
2024-10-05 20:11:04 -0300

    Problems

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -107,5 +107,175 @@ class ProblemsController < ApplicationController
   def self.single_number(nums)
     (nums.uniq.sum * 2) - nums.sum
   end
+  
+  def self.string_challenge(current_char)
+    require 'net/http'
+    require 'json'
 
+    url = 'https://coderbyte.com/api/challenges/json/age-counting'
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+
+    data = JSON.parse(response)['data']
+
+    count = 0
+    data.split(',').each do |item|
+      if item.include?('age=') && item.split('=')[1].to_i >= 50
+        count += 1
+      end
+    end
+
+    puts count
+  end
+  
+  def self.array_challenge(strArr)
+    matrix = strArr.map { |s| s[1..-2].split(',').map(&:to_i) }
+    tasks = matrix.transpose
+    pairs = []
+  
+    tasks.each_with_index do |task, i|
+      min_cost = Float::INFINITY
+      min_machine = nil
+  
+      task.each_with_index do |cost, j|
+        if cost < min_cost
+          unless pairs.include?(j)
+            min_cost = cost
+            min_machine = j
+          end
+        end
+      end
+  
+      pairs[i] = min_machine
+    end
+  
+    pairs.map.with_index { |machine, i| "(#{i + 1}-#{machine + 1})" }.join
+  end
+  
+  def self.array_challenge_2(strArr)
+    s1, s2 = strArr
+    m = s1.length
+    n = s2.length
+    
+    memo = Array.new(m+1) { Array.new(n+1, 0) }
+    
+    (1..m).each do |i|
+      (1..n).each do |j|
+        if s1[i-1] == s2[j-1]
+          memo[i][j] = memo[i-1][j-1] + 1
+        else
+          memo[i][j] = [memo[i][j-1], memo[i-1][j]].max
+        end
+      end
+    end
+    return memo[m][n]
+  end
+  
+  def self.string_challenge_1(str)
+    decrypted_str = ""
+    i = 0
+  
+    while i < str.length - 1
+      current_char = str[i]
+      next_char = str[i + 1]
+  
+      if current_char == next_char
+        decrypted_str += str[i + 2]
+        i += 3
+        next
+      end
+  
+      if next_char.ord > current_char.ord
+        direction = 1
+        start_char = current_char
+        end_char = next_char
+      else
+        direction = -1
+        start_char = next_char
+        end_char = current_char
+      end
+  
+      decrypted_str += start_char
+      current = start_char.ord
+  
+      while current != end_char.ord
+        current += direction
+        decrypted_str += current.chr
+      end
+  
+      i += 2
+    end
+  
+    decrypted_str += str[-1] unless str[-1] == "S"
+    return decrypted_str
+  end
+  
+  def sequence_break(result, start, ending, forward, backward)
+    if forward
+      if !result.empty? && result[result.length - 1] == (start - 1).chr
+        result << (ending + 1).chr
+      else
+        result << (start - 1).chr << (ending + 1).chr
+      end
+      forward = false
+    elsif backward
+      if !result.empty? && result[result.length - 1] == (start + 1).chr
+        result << (ending - 1).chr
+      else
+        result << (start + 1).chr << (ending - 1).chr
+      end
+      backward = false
+    end
+  end
+  
+  def self.alphabet_run_encryption(str)
+    i = 0
+    result = ""
+
+    while i < str.length - 1 do
+      if str[i] == str[i + 1]
+        result += (str[i + 1].ord - 1).chr + "R" + str[i + 2]
+        i += 2
+      elsif str[i] == str[i + 1].ord - 1 && i < str.length - 2 && str[i + 2] != 'S'
+        result += str[i]
+        i += 1
+      elsif str[i] == str[i + 1].ord + 1 && i < str.length - 2 && str[i + 2] != 'S'
+        result += str[i]
+        i += 1
+      elsif str[i] == str[i + 1].ord - 1 && i == str.length - 2
+        result += str[i]
+        i += 1
+      elsif str[i] == str[i + 1].ord + 1 && i == str.length - 2
+        result += str[i]
+        i += 1
+      elsif str[i] == str[i + 1].ord - 1 && i < str.length - 2 && str[i + 2] == 'S'
+        result += str[i] + str[i + 1]
+        i += 3
+      elsif str[i] == str[i + 1].ord + 1 && i < str.length - 2 && str[i + 2] == 'S'
+        result += str[i] + str[i + 1]
+        i += 3
+      elsif str[i] != str[i + 1] && str[i + 1] != str[i + 2]
+        alphabet = ""
+        if str[i] < str[i + 1]
+          alphabet = (str[i]..str[i + 1]).to_a.join('')
+        else
+          alphabet = (str[i + 1]..str[i]).to_a.reverse.join('')
+        end
+
+        offset = str[i + 2] == 'S' ? 3 : 2
+        direction = str[i + 1] < str[i] ? -1 : 1
+        idx = (alphabet.index(str[i]) + direction) % 26
+        result += alphabet[idx] + str[i + offset] if !str[i+offset].nil?
+
+        i += offset
+      end
+    end
+
+    if i == str.length - 1
+      result += str[i]
+    end
+
+    return result
+  end
+    
 end
